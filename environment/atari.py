e16a7d5de54dda2aef3c17dc444f2382d792883c
trungpq <trungpq.ml@gmail.com>
2020-07-02 11:02:40 +0800

    Adding environment normalization wrapper

--- environment/atari.py
+++ environment/atari.py
@@ -35,3 +35,22 @@ class AtariRescale(gym.ObservationWrapper):
 
     def observation(self, observation):
         return process_frame_84(observation, self.conf)
+
+
+class NormalizedEnv(gym.ObservationWrapper):
+    def __init__(self, env=None):
+        gym.ObservationWrapper.__init__(self, env)
+        self.state_mean = 0
+        self.state_std = 0
+        self.alpha = 0.9999
+        self.num_steps = 0
+
+    def observation(self, observation):
+        self.num_steps += 1
+        self.state_mean = self.state_mean * self.alpha + observation.mean() * \
+            (1-self.alpha)
+        self.state_std = self.state_std * self.alpha + observation.std()*(1-self.alpha)
+        unbiased_mean = self.state_mean/(1-pow(self.alpha, self.num_steps))
+        unbiased_std = self.state_std/(1-pow(self.alpha, self.num_steps))
+        return (observation-unbiased_mean)/(unbiased_std+1e-8)
+        
