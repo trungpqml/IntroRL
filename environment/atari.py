81c67e92d25b17d598f20f728996ce1a97c17e71
trungpq <trungpq.ml@gmail.com>
2020-07-02 10:50:02 +0800

    Adding preprocess class

--- environment/atari.py
+++ environment/atari.py
@@ -2,6 +2,8 @@ import gym
 import atari_py
 import random
 import numpy as np
+import cv2
+from gym.spaces.box import Box
 
 
 class ClipRewardEnv(gym.RewardWrapper):
@@ -12,3 +14,24 @@ class ClipRewardEnv(gym.RewardWrapper):
         """Clip rewards to be either -1, 0, or +1 based on the sign
         """
         return np.sign(reward)
+
+
+def process_frame_84(frame, conf):
+    frame = frame[conf['crop1']:conf['crop2']+160, :160]
+    frame = frame.mean(2)
+    frame = frame.astype(np.float32)
+    frame *= (1.0/255.0)
+    frame = cv2.resize(frame, (84, conf["dimension2"]))
+    frame = cv2.resize(frame, (84, 84))
+    frame = np.reshape(frame, [1, 84, 84])
+    return frame
+
+
+class AtariRescale(gym.ObservationWrapper):
+    def __init(self, env, env_conf):
+        gym.ObservationWrapper.__init__(self, env)
+        self.observation_space = Box(0.0, 1.0, [1, 84, 84])
+        self.conf = env_conf
+
+    def observation(self, observation):
+        return process_frame_84(observation, self.conf)
