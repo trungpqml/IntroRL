c28c5e49cb310603c0d60ecf5fd1da254f4ded7d
trungpq <trungpq.ml@gmail.com>
2020-07-04 16:28:47 +0800

    Adding MaxAndSkipEnv class
    Class to step through the environment at a specific rate

--- environment/atari.py
+++ environment/atari.py
@@ -131,3 +131,33 @@ class EpisodicLifeEnv(gym.Wrapper):
             obs, _, _, info = self.env.step()
             self.lives = info['ale.lives']
         return obs
+
+
+class MaxAndSkipEnv(gym.Wrapper):
+    def __init__(self, env=None, skip=4):
+        """Return only when `skip`-th frame
+        """
+        gym.Wrapper.__init__(self, env)
+        self._obs_buffer = deque(maxlen=2)
+        self._skip = skip
+
+    def step(self, action):
+        total_reward = 0.0
+        done = None
+        for _ in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            self._obs_buffer.append(obs)
+            total_reward += reward
+            if done:
+                break
+
+        max_frame = np.max(np.stack(self._obs_buffer), axis=0)
+        return max_frame, total_reward, done, info
+
+    def reset(self):
+        """Clear past frame buffer and init to first obs from inner env
+        """
+        self._obs_buffer.clear()
+        obs = self.env.reset()
+        self._obs_buffer.append(obs)
+        return obs
