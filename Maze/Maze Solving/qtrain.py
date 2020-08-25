07cf967634e6626ebdc6acc2eb107dc0bc7b6464
trungpq <trungpq.ml@gmail.com>
2020-08-25 21:06:28 +0800

    WIP

--- Maze/Maze Solving/qtrain.py
+++ Maze/Maze Solving/qtrain.py
@@ -1,6 +1,10 @@
 from helpers import format_time
 from config import cfg
 import datetime
+from qmaze import Qmaze
+from experience import Experience
+import numpy as np
+import random
 
 
 def qtrain(model, maze, **opt):
@@ -10,3 +14,36 @@ def qtrain(model, maze, **opt):
     weights_file = opt.get('weights_file', cfg.weights_file)
     name = opt.get('name', 'model')
     start_time = datetime.datetime.now()
+
+    if weights_file:
+        print(f'loading weights from file: {weights_file}')
+        model.load_weights(weights_file)
+
+    qmaze = Qmaze(maze)
+
+    experience = Experience(model, max_memory=max_memory)
+
+    win_history = []
+    n_free_cells = len(qmaze.free_cells)
+    hsize = qmaze.maze.size//2
+    win_rate = 0.0
+    imctr = 1
+
+    for epoch in range(n_epoch):
+        loss = 0.0
+        rat_cell = random.choice(qmaze.free_cells)
+        qmaze.reset(rat_cell)
+        game_over = False
+
+        envstate = qmaze.observe()
+
+        n_episodes = 0
+        while not game_over:
+            valid_actions = qmaze.valid_actions()
+            if not valid_actions:
+                break
+            prev_envstate = envstate
+            if np.random.rand() < cfg.epsilon:
+                action = ra
+            else:
+                action = np.argmax(experience.predict(envstate))
